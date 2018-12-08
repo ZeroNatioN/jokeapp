7a8644957158ff530c09b5cc7266716070d895b1
Michał Krause <michal.krause01@gmail.com>
2018-12-08 06:35:57 +0100

    Tested different configuration methods(XML, Java based).

---
+++ src/main/java/com/michalkrause/jokeapp/config/ChuckConfiguration.java
@@ -0,0 +1,12 @@
+package com.michalkrause.jokeapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+//@Configuration
+public class ChuckConfiguration {
+
+    //@Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+}
